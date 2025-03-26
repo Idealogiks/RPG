4afed9a45449a35835a4e251ae81bf72c5bc4ab2
Idea <bernard.gladys22@gmail.com>
2025-03-26 16:19:51 +0100

    fix class

--- fonction/start/startNewGame.js
+++ fonction/start/startNewGame.js
@@ -1,21 +1,18 @@
-const { createCharacter } = require('./createCharacter');
-const { validateName } = require('./validateName');
-const { askQuestion } = require('./askQuestion');
-
 const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const { askQuestion } = require('./askQuestion');
+const { createCharacter } = require('./createCharacter');
 
 async function startNewGame() {
-  console.log("Démarrage d’une nouvelle partie !");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
   try {
     const name = await askQuestion(rl, "Entrez le nom de votre personnage (3-20 lettres) : ");
-    validateName(name); 
+    const className = await askQuestion(rl, "Choisissez une classe (Guerrier/Mage/Voleur) : ");
 
-    const characterClass = await askQuestion(rl, "Choisissez une classe (Guerrier/Mage/Voleur) : ");
-    const hero = createCharacter(name, characterClass); 
+    const hero = createCharacter(name, className);
 
     console.log("\nPersonnage créé avec succès !\n");
     console.log(hero.getSummary());
